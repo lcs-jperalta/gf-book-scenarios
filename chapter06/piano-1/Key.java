2577ede1babc4a8f663e5596501fed0365db2ed5
Joven Peralta <jperalta@lcs.on.ca>
2019-04-13 10:33:57 -0400

    Fixed bug, sound plays once when key is pressed

--- chapter06/piano-1/Key.java
+++ chapter06/piano-1/Key.java
@@ -3,10 +3,20 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
 public class Key extends Actor
 {
     /**
+     * Instance variables
+     * These can be used anywhere in the class below
+     */
+    //Will keep track of whether a key was just pressed
+    private boolean keyAlreadyDown;
+    
+    
+    /**
      * Create a new key.
      */
     public Key()
     {
+        //No key has been pressed
+        keyAlreadyDown = false;
     }
 
     /**
@@ -15,16 +25,24 @@ public class Key extends Actor
     public void act()
     {
         //Animate the piano key being pressed
-        if ( Greenfoot.isKeyDown("g") )
+        // Condition 1 - is the g key being pressed
+        // Condition 2 - was the g key already down
+        if ( Greenfoot.isKeyDown("g") && keyAlreadyDown == false )
         {
             //Key is down
             setImage("white-key-down.png");
             play();
+            keyAlreadyDown = true;
         }    
-        else
+        
+        // Stop showing the key being pressed
+        // Condition 1 - the key was down the last time act() fired
+        // Condition 2 - the "g" key has been released
+        if ( (keyAlreadyDown == true) && Greenfoot.isKeyDown("g") == false)
         {
             //Key is up
             setImage("white-key.png");
+            keyAlreadyDown = false;
         }
 
     }
