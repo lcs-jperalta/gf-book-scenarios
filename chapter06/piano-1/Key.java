48ed322872dcd5086572efd8a27a380d49840ecb
Joven Peralta <jperalta@lcs.on.ca>
2019-04-16 13:16:01 -0400

    created

--- chapter06/piano-1/Key.java
+++ chapter06/piano-1/Key.java
@@ -8,15 +8,21 @@ public class Key extends Actor
      */
     //Will keep track of whether a key was just pressed
     private boolean keyAlreadyDown;
-    
-    
+
+    //Will contain the key and sound file that this instance of the class will play
+    private String key;
+    private String sound;
     /**
      * Create a new key.
      */
-    public Key()
+    public Key(String keyName, String soundFile)
     {
         //No key has been pressed
         keyAlreadyDown = false;
+
+        // Set the instance variables with the paramaters
+        key = keyName;
+        sound = soundFile;
     }
 
     /**
@@ -27,18 +33,18 @@ public class Key extends Actor
         //Animate the piano key being pressed
         // Condition 1 - is the g key being pressed
         // Condition 2 - the key was not already down
-        if ( Greenfoot.isKeyDown("g") && !keyAlreadyDown )
+        if ( Greenfoot.isKeyDown(key) && !keyAlreadyDown )
         {
             //Key is down
             setImage("white-key-down.png");
             play();
             keyAlreadyDown = true;
         }    
-        
+
         // Stop showing the key being pressed
         // Condition 1 - the key was down the last time act() fired
         // Condition 2 - the "g" key has been released
-        if ( (keyAlreadyDown == true) && !Greenfoot.isKeyDown("g") )
+        if ( (keyAlreadyDown == true) && !Greenfoot.isKeyDown(key) )
         {
             //Key is up
             setImage("white-key.png");
@@ -52,7 +58,7 @@ public class Key extends Actor
      */
     public void play()
     {
-        Greenfoot.playSound("3a.wav");
+        Greenfoot.playSound(sound + ".wav");
     }
 }
 
